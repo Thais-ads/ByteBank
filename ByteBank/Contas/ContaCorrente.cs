ba0e4143eb6f989bd9105d629a2864e379516ed4
thais.spinoza <thais.spinoza@SLAP0009>
2023-10-19 22:26:42 -0300

    Add project files.

---
+++ ByteBank/Contas/ContaCorrente.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Titular;
+
+namespace ByteBank.Contas
+{
+
+    //definição de uma classe com os seus atributos publicos e visiveis; 
+    public class ContaCorrente
+    {
+
+        public int numero_agencia;
+
+        public Cliente titular;
+
+        public string conta;
+
+        public double saldo;
+
+        //criando um Medodo ;
+        public void Depositar(double valor)
+        {
+            saldo += valor;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (valor <= saldo)
+            {
+
+                saldo -= valor;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        public bool Transferir(double valor, ContaCorrente transferencia)
+        {
+            if (saldo <= valor)
+            {
+                Console.WriteLine("Transferencia insuficiente;");
+                return false;
+
+            }
+            else
+            {
+                //double recebe = this.saldo - valor;
+                //transferencia.saldo += valor;
+                //this.saldo = recebe; 
+
+                Sacar(valor);
+                transferencia.Depositar(valor);
+
+                return true;
+            }
+        }
+    }
+
+
+
+
+}
---
+++ ByteBank/Program.cs
@@ -0,0 +1,75 @@
+﻿using ByteBank.Contas;
+using ByteBank.Titular;
+
+//definindo uma classe do objeto ContaCorrente.
+
+Cliente cliente = new Cliente();
+cliente.Nome = "";
+cliente.profissa = "";
+cliente.cpf = "";
+
+
+ContaCorrente Pedro = new ContaCorrente();
+
+
+Pedro.numero_agencia = 1;
+Pedro.titular = cliente;
+Pedro.conta = "2TDSPJ";
+Pedro.saldo = 0;
+
+
+
+Pedro.numero_agencia = 1;
+Pedro.titular = cliente;
+Pedro.conta = "2TDSPJ";
+Pedro.saldo = 0;
+
+
+//testando a referencia ..
+ContaCorrente conta = new ContaCorrente();
+
+conta.titular = new Cliente();
+
+conta.numero_agencia = 1;
+conta.titular.Nome = "jose";
+conta.titular.profissa = "tester";
+conta.titular.cpf = "49397316850";
+conta.conta = "2TDSPJ";
+conta.saldo = 10;
+
+
+Console.WriteLine($"A agencia de numero: {conta.numero_agencia}," +
+                  $" titular: {conta.titular}," +
+                  $" conta: {conta.conta}," +
+                  $" saldo de: {conta.saldo}");
+
+
+conta.Depositar(100);
+
+Console.WriteLine($"A conta da thais é de: {conta.saldo}");
+
+
+
+if (conta.Sacar(300) == true)
+    {
+    Console.WriteLine($"Saldo pos-saque: {conta.saldo}");
+    }
+else
+{
+    Console.WriteLine($"Saldo insuficiente para saque");
+}
+
+Console.WriteLine($" O Saldo atual da thais antes-transferencia é: {conta.saldo}");
+
+conta.Transferir(90, Pedro);
+
+Console.WriteLine($" O Saldo atual do pedro é: {Pedro.saldo}");
+Console.WriteLine($" O Saldo atual da thais pos-transferencia é: {conta.saldo}");
+
+
+
+ContaCorrente contaDoPedro = new ContaCorrente();
+Console.WriteLine(contaDoPedro.titular);
+Console.WriteLine(contaDoPedro.conta);
+Console.WriteLine(contaDoPedro.numero_agencia);
+Console.WriteLine(contaDoPedro.saldo);
